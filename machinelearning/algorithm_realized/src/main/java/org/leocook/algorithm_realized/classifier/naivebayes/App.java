6928e0130c29d592a9f82b419e329f2e9984f154
leocook <leocook@163.com>
2015-07-05 23:11:46 +0800

    第一次提交

---
+++ machinelearning/algorithm_realized/src/main/java/org/leocook/algorithm_realized/classifier/naivebayes/App.java
@@ -0,0 +1,11 @@
+package org.leocook.algorithm_realized.classifier.naivebayes;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
